c6a6772a3b5611413d96b650d45f68e5ea68664a
Tiger Caldwell <caldwet6@wwu.edu>
2024-01-31 01:43:16 -0800

    Split into x86_64 and i686 packages Added a formatter

--- flake.nix
+++ flake.nix
@@ -1,31 +1,39 @@
 {
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in rec {
-    packages."${system}".default = pkgs.pkgsi686Linux.stdenv.mkDerivation rec {
-      name = "blitz";
-      src = pkgs.fetchFromGitHub {
+  outputs = { self, nixpkgs }:
+    let
+      derivInfo = pkgs: rec {
+        name = "blitz";
+        src = pkgs.fetchFromGitHub {
           owner = "BlitzOSProject";
           repo = "BlitzOSProject.github.io";
           rev = "3c60e8e4ac2073ba94802712125b429b2b5ffc49";
           hash = "sha256-DJWV0vBdDGPBylapwbFjLZyybse+Nw8DSEpM07arbdU=";
+        };
+        installPhase = ''
+          mkdir -p $out/bin
+          cd ${src}/BlitzBin/Ubuntu64
+          cp kpl asm lddd blitz diskUtil dumpObj hexdump check endian $out/bin
+          chmod +x $out/bin/*
+        '';
+        preFixup = ''
+          cd $out/bin
+            for file in ./*
+            do
+              patchelf \
+                --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+                $file
+            done
+        '';
       };
-      installPhase = ''
-        mkdir -p $out/bin
-        cd ${src}/BlitzBin/Ubuntu64
-        cp kpl asm lddd blitz diskUtil dumpObj hexdump check endian $out/bin
-        chmod +x $out/bin/*
-      '';
-      preFixup = ''
-      cd $out/bin
-        for file in ./*
-        do
-          patchelf \
-            --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-            $file
-        done
-      '';
+    in rec {
+      packages."x86_64-linux".default =
+        let pkgs = import nixpkgs { system = "x86_64-linux"; };
+        in pkgs.pkgsi686Linux.stdenv.mkDerivation (derivInfo pkgs);
+      packages."i686-linux".default =
+        let pkgs = import nixpkgs { system = "i686-linux"; };
+        in pkgs.stdenv.mkDerivation (derivInfo pkgs);
+
+      formatter.x86_64-linux =
+        (import nixpkgs { system = "x86_64-linux"; }).nixfmt;
     };
-  };
 }
