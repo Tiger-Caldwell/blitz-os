cd80b76fcbc15df7d7f1e89ce7776a96df1f0f60
Tiger Caldwell <caldwet6@wwu.edu>
2024-01-31 01:43:16 -0800

    Working

--- flake.nix
+++ flake.nix
@@ -2,26 +2,30 @@
   outputs = { self, nixpkgs }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
-    blitz = pkgs.stdenv.mkDerivation rec {
-        name = "blitz";
-        src = pkgs.fetchFromGitHub {
-            owner = "BlitzOSProject";
-            repo = "BlitzOSProject.github.io";
-            rev = "3c60e8e4ac2073ba94802712125b429b2b5ffc49";
-            hash = "sha256-DJWV0vBdDGPBylapwbFjLZyybse+Nw8DSEpM07arbdU=";
-        };
-        nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-        installPhase = ''
-            runHook preInstall
-            mkdir -p $out/bin
-            install -m755 -D ${src}/BlitzBin/Ubuntu64/* $out/bin
-            rm $out/bin/blitztools.tar $out/bin/index.html
-            runHook postInstall
-        '';
-    };
-  in {
-    devShells."${system}".default = pkgs.mkShell {
-        buildInputs = [ blitz ];
+  in rec {
+    packages."${system}".default = pkgs.pkgsi686Linux.stdenv.mkDerivation rec {
+      name = "blitz";
+      src = pkgs.fetchFromGitHub {
+          owner = "BlitzOSProject";
+          repo = "BlitzOSProject.github.io";
+          rev = "3c60e8e4ac2073ba94802712125b429b2b5ffc49";
+          hash = "sha256-DJWV0vBdDGPBylapwbFjLZyybse+Nw8DSEpM07arbdU=";
+      };
+      installPhase = ''
+        mkdir -p $out/bin
+        cd ${src}/BlitzBin/Ubuntu64
+        cp kpl asm lddd blitz diskUtil dumpObj hexdump check endian $out/bin
+        chmod +x $out/bin/*
+      '';
+      preFixup = ''
+      cd $out/bin
+        for file in ./*
+        do
+          patchelf \
+            --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+            $file
+        done
+      '';
     };
   };
 }
